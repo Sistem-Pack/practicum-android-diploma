d51d0980770a3be1686988dde2a4851643f13ad0
Alexandr <56019760+Sistem-Pack@users.noreply.github.com>
2024-03-31 23:20:50 +0500

    Добавлены следующие элементы: 1. BottomNavigationMenu 2. Корректировка тем и наследования в темах 3. Иконки для BottomNavigationMenu 4. Отдельные дневные/ночные ресурсы для BottomNavigationMenu 5. Добавлены строковые ресурсы для BottomnavigationMenu 6. Добавлен 1 элемент размера в 1dp в ресурсы размеров

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -1,21 +1,40 @@
 package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import ru.practicum.android.diploma.BuildConfig
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+    private val binding: ActivityRootBinding by lazy { ActivityRootBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        setContentView(binding.root)
 
-        // Пример использования access token для HeadHunter API
-        networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
-    }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.root_fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
 
-    private fun networkRequestExample(accessToken: String) {
-        // ...
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.teamPageFragment,
+                R.id.favoritesFragment,
+                R.id.mainFragment -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                    binding.bottomNavigationViewLineTop.visibility = View.VISIBLE
+                }
+
+                else -> {
+                    binding.bottomNavigationView.visibility = View.GONE
+                    binding.bottomNavigationViewLineTop.visibility = View.GONE
+                }
+            }
+        }
+
     }
 
 }
