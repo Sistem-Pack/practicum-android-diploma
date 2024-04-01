248d668e0c1e1a51b85261a7834387ef779cbc39
Babaylov7 <babaylov-ayu@mail.ru>
2024-04-01 16:04:03 +0500

    E0-I6 Убрал еще пробелы и поставил по пробелу после //...

--- app/src/main/java/ru/practicum/android/diploma/data/db/entity/VacancyEntity.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/entity/VacancyEntity.kt
@@ -7,40 +7,40 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "vacancy_table")
 data class VacancyEntity(
     @PrimaryKey @ColumnInfo(name = "vacancy_id")
-    val vacancyId: String,                    //ID вакансии
+    val vacancyId: String,  // ID вакансии
     @ColumnInfo(name = "vacancy_name")
-    val vacancyName: String,                //Название проффесии
+    val vacancyName: String, // Название проффесии
     @ColumnInfo(name = "industry")
-    val industry: String,                    //Отрасль
+    val industry: String, // Отрасль
     @ColumnInfo(name = "country")
-    val country: String,                    //Страна
+    val country: String, // Страна
     @ColumnInfo(name = "area_id")
-    val areaId: String,                        //Id региона
+    val areaId: String, // Id региона
     @ColumnInfo(name = "area_region")
-    val areaRegion: String,                    //Регион
+    val areaRegion: String, // Регион
     @ColumnInfo(name = "contacts-email")
-    val contactsEmail: String,                //Контакты Эл.почта
+    val contactsEmail: String, // Контакты Эл.почта
     @ColumnInfo(name = "contacts-name")
-    val contactsName: String,                //Контактное лицо
+    val contactsName: String, // Контактное лицо
     @ColumnInfo(name = "contacts-phones")
-    val contactsPhones: String,                //Контакты Телефоны
+    val contactsPhones: String, // Контакты Телефоны
     @ColumnInfo(name = "description")
-    val description: String,                //Описание вакансии
+    val description: String, // Описание вакансии
     @ColumnInfo(name = "employment_name")
-    val employmentName: String,                //Тип занятости
+    val employmentName: String, // Тип занятости
     @ColumnInfo(name = "experience_name")
-    val experienceName: String,                //Опыт работы
+    val experienceName: String, // Опыт работы
     @ColumnInfo(name = "salary_currency")
-    val salaryCurrency: String,                //ЗП Валюта
+    val salaryCurrency: String, // ЗП Валюта
     @ColumnInfo(name = "salary_from")
-    val salaryFrom: Int,                    //ЗП от
+    val salaryFrom: Int, // ЗП от
     @ColumnInfo(name = "salary_to")
-    val salaryTo: Int,                        //ЗП до
+    val salaryTo: Int, // ЗП до
     @ColumnInfo(name = "key_skills")
-    val keySkills: String,                    //Ключевые обязанности
+    val keySkills: String, // Ключевые обязанности
     @ColumnInfo(name = "artwork_url")
-    val artworkUrl: String,                    //Изображение
+    val artworkUrl: String, // Изображение
     @ColumnInfo(name = "is_favorite")
-    val isFavorite: Boolean                    //В избранном
+    val isFavorite: Boolean // В избранном
 
 )
