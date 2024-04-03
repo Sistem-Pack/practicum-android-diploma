0af6ebe13a6a46a738b62ab0d1c53ebde7ca2e14
Babaylov7 <babaylov-ayu@mail.ru>
2024-04-03 10:13:00 +0500

    E1-I21 Сделал data class VacancyDetails - все необходимые поля для экрана Вакансия. data class VacancyShort - поля необходимые для отражения списка результатов поиска.

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/VacancyDetails.kt
@@ -0,0 +1,23 @@
+package ru.practicum.android.diploma.domain.models
+
+data class VacancyDetails(
+    val vacancyId: String, // ID вакансии
+    val vacancyName: String, // Название проффесии
+    val employer: String, // Работадатель
+    val industry: String, // Отрасль
+    val country: String, // Страна
+    val areaId: String, // Id региона
+    val areaRegion: String, // Регион
+    val contactsEmail: String, // Контакты Эл.почта
+    val contactsName: String, // Контактное лицо
+    val contactsPhones: String, // Контакты Телефоны
+    val description: String, // Описание вакансии
+    val employmentName: String, // Тип занятости
+    val experienceName: String, // Опыт работы
+    val salaryCurrency: String?, // ЗП Валюта
+    val salaryFrom: String?, // ЗП от
+    val salaryTo: String?, // ЗП до
+    val keySkills: String, // Ключевые обязанности
+    val artworkUrl: String, // Изображение
+    var isFavorite: Boolean = false // В избранном
+)
---
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/VacancyShort.kt
@@ -0,0 +1,12 @@
+package ru.practicum.android.diploma.domain.models
+
+data class VacancyShort (
+    val vacancyId: String, // ID вакансии
+    val vacancyName: String, // Название проффесии
+    val employer: String, // Работадатель
+    val areaRegion: String, // Регион (в документации НН указано название города "Москва")
+    val salaryCurrency: String?, // ЗП Валюта
+    val salaryFrom: String?, // ЗП от
+    val salaryTo: String?, // ЗП до
+    val artworkUrl: String, // Изображение
+)
