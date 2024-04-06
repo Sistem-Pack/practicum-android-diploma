baa9cc22705b0c8fc34b8f84580591ce99359d43
DaniilKosmachev <fame37@yandex.ru>
2024-04-06 20:57:33 +0300

    Исправление detekt

--- app/src/main/java/ru/practicum/android/diploma/presentation/main/MainViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/main/MainViewModel.kt
@@ -3,58 +3,9 @@ package ru.practicum.android.diploma.presentation.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import ru.practicum.android.diploma.domain.models.ResponseStatus
-import ru.practicum.android.diploma.domain.models.VacancySearchResult
-import ru.practicum.android.diploma.domain.search.VacancyInteractor
 
-class MainViewModel(
-    val vacancyInteractor: VacancyInteractor
-) : ViewModel() {
-    private var requestText = ""
-    private var isClickAllowed = true
-    private var clickJob: Job? = null
-    private var searchJob: Job? = null
+class MainViewModel : ViewModel() {
 
-    private val _liveData = MutableLiveData<VacancySearchResult>()
-    val liveData: LiveData<VacancySearchResult> = _liveData
-
-    fun changeRequestText(text: String) {
-        requestText = text
-    }
-
-    fun searchDebounce() {
-        searchJob?.cancel()
-        searchJob = viewModelScope.launch {
-            delay(2000L)
-            search()
-        }
-    }
-
-    private fun search() {
-        _liveData.postValue(getLoadingStatus())
-        sendRequest()
-    }
-
-    private fun sendRequest() {
-        viewModelScope.launch {
-            vacancyInteractor
-                .searchVacancy(requestText)
-                .collect { result ->
-                    _liveData.postValue(result)
-                }
-        }
-    }
-    private fun getLoadingStatus(): VacancySearchResult {
-        return VacancySearchResult(
-            results = emptyList(),
-            ResponseStatus.DEFAULT,
-            0,
-            0,
-            0
-        )
-    }
+    private val _liveData = MutableLiveData<String>()
+    val liveData: LiveData<String> = _liveData
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/main/MainFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/main/MainFragment.kt
@@ -1,26 +1,17 @@
 package ru.practicum.android.diploma.ui.main
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentMainBinding
-import ru.practicum.android.diploma.domain.models.vacancy.Vacancy
 import ru.practicum.android.diploma.presentation.main.MainViewModel
 
 class MainFragment : Fragment() {
 
     private var binding: FragmentMainBinding? = null
-    private var editTextValue = ""
-
-    // private lateinit var adapter: VacancyAdapter
-    private var vacancies = ArrayList<Vacancy>()
 
     private val viewModel by viewModel<MainViewModel>()
 
@@ -31,45 +22,10 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        adapter = VacancyAdapter(vacancies)
-//
-//        adapter.itemClickListener = {it ->
-//            //запустить фрагмент с деталями вакансии и проверить кликдебаунс
-//            //startJobVacancyFragment(it.vacancyId)
-//        }
-
-        val simpleTextWatcher = object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                editTextValue = binding!!.etSearch.text.toString()
-                if (editTextValue.isEmpty()) {
-                    // viewModel.onDestroy()
-                } else {
-                    viewModel.changeRequestText(binding!!.etSearch.text.toString())
-                    viewModel.searchDebounce()
-                }
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-            }
-        }
-        binding!!.etSearch.addTextChangedListener(simpleTextWatcher)
     }
 
     override fun onDestroyView() {
         binding = null
         super.onDestroyView()
-    }
-
-    private fun startJobVacancyFragment(vacancyId: String) {
-        val bundle = Bundle()
-        bundle.putString(VACANCY_ID, vacancyId)
-        findNavController().navigate(R.id.action_mainFragment_to_jobVacancyFragment, bundle)
-    }
-
-    companion object {
-        private const val VACANCY_ID = "vacancy_id"
     }
 }
