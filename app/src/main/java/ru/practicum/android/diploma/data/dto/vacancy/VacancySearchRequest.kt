f1660e0135f42122444481d9a090d127d7c24176
DaniilKosmachev <fame37@yandex.ru>
2024-04-07 14:44:04 +0300

    В HHApi изменен метод поиска (пока не реализованы фильтры - поиск по нескольким query parameters)
    В data-классе VacancySearchRequest добавлены новые поля для осуществления запроса (page - запросит страницу, perPage - устанавливает количество элементов на странице, если не указывать, то по дефолту - 10)
    
    В реализации RetrofitNetworkClient в запрос добалвены поля VacancySearchRequest

--- app/src/main/java/ru/practicum/android/diploma/data/dto/vacancy/VacancySearchRequest.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/vacancy/VacancySearchRequest.kt
@@ -1,5 +1,7 @@
 package ru.practicum.android.diploma.data.dto.vacancy
 
 data class VacancySearchRequest(
-    val expression: String
+    val expression: String,
+    val page: Int,
+    val perPage: Int = 20
 )
