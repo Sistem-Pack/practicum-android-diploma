b461fc5cb321b8710c8e7464639aebb36c5969fe
Eka666Sed <cat.sedoff@gmail.com>
2024-04-08 13:36:52 +0200

    E2-I30 Добавляет некоторые методы класса JobVacancyFragment.kt и JobVacancyViewModel.kt

--- app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/JobVacancyViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/JobVacancyViewModel.kt
@@ -3,9 +3,38 @@ package ru.practicum.android.diploma.presentation.vacancy
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import ru.practicum.android.diploma.domain.db.FavoriteVacanciesInteractor
+import ru.practicum.android.diploma.domain.models.ResponseStatus
+import ru.practicum.android.diploma.domain.models.vacancy.VacancyDetails
 
-class JobVacancyViewModel : ViewModel() {
+class JobVacancyViewModel(private val favoriteVacancyInteractor: FavoriteVacanciesInteractor) : ViewModel() {
 
     private val _liveData = MutableLiveData<String>()
     val liveData: LiveData<String> = _liveData
-}
+    private var isFavourite = false
+    private val _shareUrl = MutableLiveData<String?>()
+    val shareUrl: LiveData<String?> get() = _shareUrl
+
+    fun showDetailVacancy(vacancyid: String) {
+        viewModelScope.launch {
+
+            }
+        }
+    }
+
+    fun clickToFavorite(vacancy: VacancyDetails) {
+        viewModelScope.launch(Dispatchers.IO) {
+            //isFavourite =
+            }
+
+        }
+
+    fun checkFavorite(vacancyId: VacancyDetails) {
+        viewModelScope.launch(Dispatchers.IO) {
+            //isFavourite =
+        }
+    }
+
--- app/src/main/java/ru/practicum/android/diploma/ui/vacancy/JobVacancyFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancy/JobVacancyFragment.kt
@@ -1,12 +1,17 @@
 package ru.practicum.android.diploma.ui.vacancy
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.databinding.FragmentJobVacancyBinding
+import ru.practicum.android.diploma.domain.db.FavoriteVacanciesIdState
+import ru.practicum.android.diploma.domain.models.vacancy.VacancyDetails
 import ru.practicum.android.diploma.presentation.vacancy.JobVacancyViewModel
 
 class JobVacancyFragment : Fragment() {
@@ -20,13 +25,84 @@ class JobVacancyFragment : Fragment() {
         return binding!!.root
     }
 
+    override fun onResume() {
+        super.onResume()
+        viewModel.showDetailVacancy(KEY_VACANCY)
+        observeViewModel()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //viewModel.showDetailVacancy(KEY_VACANCY)
+        binding?.group?.visibility = View.GONE
+        //observeViewModel()
+        binding?.ivBack?.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 
     override fun onDestroyView() {
         binding = null
         super.onDestroyView()
+    }
+
+
+    private fun observeViewModel() {
+        viewModel. .observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is FavoriteVacanciesIdState.SuccessfulRequest -> showDetails(it.vacancyId)
+                is FavoriteVacanciesIdState.FailedRequest -> showErrorMessage()
+            }
+        }
+    }
+
+    private fun showErrorMessage() {
+        binding.apply {
+            binding?.group?.visibility = View.INVISIBLE
+            binding?.tvServerErrorVacancyPlaceholder?.visibility = View.VISIBLE
+        }
+    }
+
+
+    private fun hideContactsIfEmpty(vacancyId: VacancyDetails) {
+            if (vacancyId.contactsName.isEmpty()) {
+                binding?.tvContactPersonValue?.visibility = View.GONE
+                binding?.tvContactPerson?.visibility = View.GONE
+            }
+            if (vacancyId.contactsEmail.isEmpty()) {
+                binding?.tvContactEmailValue?.visibility = View.GONE
+                binding?.tvContactEmail?.visibility = View.GONE
+            }
+            if (vacancyId.contactsPhones.isEmpty()) {
+                binding?.tvContactPhoneValue?.visibility = View.GONE
+                binding?.tvContactPhone?.visibility = View.GONE
+            }
+        //у нас в модели отсутствует поле комментария к вакансии!! Потерялось, надо добавить
+            if (vacancyId.contactsComment.isEmpty()) {
+                binding?.tvCommentValue?.visibility = View.GONE
+                binding?.tvComment?.visibility = View.GONE
+            }
+            if (vacancyId.contactsName.isEmpty() &&
+                vacancyId.contactsEmail.isEmpty() &&
+                vacancyId.contactsPhones.isEmpty()
+            ) {
+                binding?.ContactBox?.visibility = View.GONE
+            }
+    }
+
+    private fun showKeySkills(vacancyId: VacancyDetails) {
+            if (vacancyId.keySkills.isEmpty()) {
+                binding?.tvKeySkills?.visibility = View.GONE
+                binding?.tvKeySkillsDescription?.visibility = View.GONE
+            } else {
+                binding?.tvKeySkillsDescription?.text = vacancyId.keySkills
+            }
+    }
+
+    private fun favoriteClickInit(vacancyId: VacancyDetails) {
+        binding?.ivFavorites?.setOnClickListener {
+            viewModel.clickToFavorite(vacancyId)
+        }
     }
 
     companion object {
