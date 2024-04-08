52ec390ec5666108e161e1fa049d21c73e15121d
Babaylov7 <babaylov-ayu@mail.ru>
2024-04-08 14:28:59 +0500

    E2-I41 - в процессе создания классов

---
+++ app/src/main/java/ru/practicum/android/diploma/data/sharing/impl/SharingRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package ru.practicum.android.diploma.data.sharing.impl
+
+import ru.practicum.android.diploma.domain.sharing.SharingRepository
+
+class SharingRepositoryImpl() : SharingRepository {
+    override fun shareLink(link: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun sendEmail(email: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun makeACall(phoneNumber: String) {
+        TODO("Not yet implemented")
+    }
+}
