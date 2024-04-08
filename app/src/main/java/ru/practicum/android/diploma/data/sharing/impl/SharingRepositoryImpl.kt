26fdf46def3cf3f495f1b4e395d26caf57dafbdf
Babaylov7 <babaylov-ayu@mail.ru>
2024-04-08 15:15:15 +0500

    E2-I41 - что-то накидал. Надо верстку экрана с деталями вакансии

--- app/src/main/java/ru/practicum/android/diploma/data/sharing/impl/SharingRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/sharing/impl/SharingRepositoryImpl.kt
@@ -1,17 +1,42 @@
 package ru.practicum.android.diploma.data.sharing.impl
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import ru.practicum.android.diploma.app.App
 import ru.practicum.android.diploma.domain.sharing.SharingRepository
 
-class SharingRepositoryImpl() : SharingRepository {
+class SharingRepositoryImpl(
+    private val context: Context,
+    val app: App
+) : SharingRepository {
     override fun shareLink(link: String) {
-        TODO("Not yet implemented")
+        Intent().apply {
+            action = Intent.ACTION_SEND
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, link)
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            app.startActivity(this)
+        }
     }
 
     override fun sendEmail(email: String) {
-        TODO("Not yet implemented")
+        Intent().apply {
+            action = Intent.ACTION_SENDTO
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, email)
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            app.startActivity(Intent.createChooser(this, "Направить"))
+        }
     }
 
     override fun makeACall(phoneNumber: String) {
-        TODO("Not yet implemented")
+        Intent().apply {
+            action = Intent.ACTION_DIAL
+            data = Uri.parse("tel:$phoneNumber")
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            app.startActivity(this)
+        }
     }
 }
+
