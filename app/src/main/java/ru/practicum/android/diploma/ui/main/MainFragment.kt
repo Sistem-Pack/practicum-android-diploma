ca6ccc937d689554db7e0a9feaa67cac04cd4976
Alexandr <56019760+Sistem-Pack@users.noreply.github.com>
2024-04-11 09:05:58 +0500

    Дополнительно мелкие исправления detect

--- app/src/main/java/ru/practicum/android/diploma/ui/main/MainFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/main/MainFragment.kt
@@ -1,5 +1,6 @@
 package ru.practicum.android.diploma.ui.main
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,11 +10,10 @@ import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import android.content.Context
-import androidx.recyclerview.widget.ConcatAdapter
-import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentMainBinding
