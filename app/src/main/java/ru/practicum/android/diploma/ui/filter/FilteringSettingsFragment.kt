8e20e859d714ec1cf244268694f8254fb5fc2461
Babaylov7 <babaylov-ayu@mail.ru>
2024-04-12 12:32:55 +0500

    E4 Выполнены задачи: Done 49. Проверка корректности ввода ЗП на целочисленные значения Done 50. Кнопка очистки поля ввода ЗП если оно не пустое Done 53. При нажатии на пункт «Отрасль» пользователь переходит на экран выбора отрасли компании Done 85. При нажатии на пункт «Место работы» должен открываться экран выбора места работы(событие)

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/FilteringSettingsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/FilteringSettingsFragment.kt
@@ -1,10 +1,14 @@
 package ru.practicum.android.diploma.ui.filter
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.databinding.FragmentFilteringSettingsBinding
 import ru.practicum.android.diploma.presentation.filter.FilteringSettingsViewModel
@@ -22,10 +26,47 @@ class FilteringSettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        createClickListeners()
+        createTextWatcher()
     }
 
     override fun onDestroyView() {
         binding = null
         super.onDestroyView()
     }
+
+    private fun createClickListeners() {
+        binding!!.ivBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
+        binding!!.ivArrowRightJobPlace.setOnClickListener {
+            findNavController().navigate(
+                FilteringSettingsFragmentDirections.actionFilteringSettingsFragmentToPlacesOfWorkFragment()
+            )
+        }
+        binding!!.ivArrowRightIndustry.setOnClickListener {
+            findNavController().navigate(
+                FilteringSettingsFragmentDirections.actionFilteringSettingsFragmentToIndustrySelectionFragment()
+            )
+        }
+        binding!!.ivSalaryClear.setOnClickListener {
+            binding!!.tietSalary.text!!.clear()
+        }
+    }
+
+    private fun createTextWatcher() {
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, start: Int, before: Int, count: Int) {
+                binding!!.ivSalaryClear.isVisible = !p0.isNullOrEmpty()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        }
+        binding!!.tietSalary.addTextChangedListener(simpleTextWatcher)
+    }
 }
+
