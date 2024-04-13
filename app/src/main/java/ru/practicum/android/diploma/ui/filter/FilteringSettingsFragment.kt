c39392d8abc43afbb5ba25eda614bef083921bd8
Babaylov7 <babaylov-ayu@mail.ru>
2024-04-13 10:51:20 +0500

    E4-I49 Устранил замечания GitHub

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/FilteringSettingsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/FilteringSettingsFragment.kt
@@ -1,8 +1,6 @@
 package ru.practicum.android.diploma.ui.filter
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
